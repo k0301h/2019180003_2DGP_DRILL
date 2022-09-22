15db025410e5a8f20a885d2109e7f2492861830b
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2022-09-22 22:57:26 +0900

    drill 07 완성
    2019180003 고인호

---
+++ drill 07/anim_viewer.py
@@ -0,0 +1,54 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+space = load_image('space.png')
+sun = load_image('sun.png')
+earth = load_image('earth.png')
+moon = load_image('moon.png')
+
+earthframe = 0
+moonframe = 0
+
+def draw_all(ex, ey, mx, my, earthframe, moonframe):
+        clear_canvas()
+    # 적절한 해 스프라이트를 찾지못해 일반 png로 대채하였습니다.
+        space.draw(400, 300)
+        sun.draw(400, 300)
+    # 달 스프라이트의 시작 위치를 (0, 80)으로 설정함 (우하향), 지구도 같은이유로(0, 167)으로 설정
+        moon.clip_draw(moonframe % 4 * 20, 80 - moonframe // 4 * 20, 20, 20, mx, my) 
+        earth.clip_draw(earthframe % 5 * 33, 167 - earthframe // 5 * 33, 33, 33, ex, ey)
+        update_canvas()
+        delay(0.1)
+
+def earth_circle_calculation(cx,cy,r,deg):
+    x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+    y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+    return x,y
+
+# 공전속도는 달이 더 빠르게 제작 
+def moon_circle_calculation(cx,cy,r,deg):
+    x = cx + r * math.cos(deg / 180 * 2 * math.pi)
+    y = cy + r * math.sin(deg / 180 * 2 * math.pi)
+    return x,y
+
+def orbit_move(earthframe, moonframe):
+    ecx , ecy, er = 400, 300 ,150
+    for deg in range(-90, 270, 5):
+        ex,ey = earth_circle_calculation(ecx,ecy,er,deg)
+
+        mcx , mcy, mr = ex, ey, 50
+        mx,my = moon_circle_calculation(mcx,mcy,mr,deg)
+
+        draw_all(ex,ey,mx,my,earthframe, moonframe)
+
+        moonframe = (moonframe + 1) % 19
+        earthframe = (earthframe + 1) % 30
+
+
+while(1):
+    orbit_move(earthframe, moonframe)
+    get_events()
+
+close_canvas()
