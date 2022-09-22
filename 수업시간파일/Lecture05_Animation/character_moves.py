526d6cbeb085a548ef2d753f91182f7f416dff81
inho <dlsghdmsqlc@naver.com>
2022-09-22 13:07:27 +0900

    2022-09-22일 수업 완료

---
+++ 수업시간파일/Lecture05_Animation/character_moves.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+for x in range(0, 800,5):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, 90)
+    x = x + 2
+    update_canvas()
+    delay(0.01)
+    get_events()
+
+close_canvas()
